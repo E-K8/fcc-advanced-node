9d8395acc22e03ff2523ae9950bebc1b9be2bedd
E-K8 <ekatplat@gmail.com>
2024-04-18 14:59:46 +0100

    set up a template engine, render a pug page

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+let pug = require('pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 
+app.route('/').get((req, res) => {
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
