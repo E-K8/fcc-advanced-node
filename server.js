8aec25826a972307cebf651c5f68b1c6b6228c69
E-K8 <ekatplat@gmail.com>
2024-04-24 16:51:03 +0100

    set up passport

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 let pug = require('pug');
+const passport = require('passport');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+passport.initialize();
+passport.session();
+
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
 app.route('/').get((req, res) => {
+  req.session.count++;
+  console.log(req.session);
   res.render('index', { title: 'Hello', message: 'Please log in' });
 });
 
