bf4b401b11b6ba3aaac037396da14e14321a0226
E-K8 <ekatplat@gmail.com>
2024-04-27 21:55:02 +0100

    Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const session = require('express-session');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const myDB = require('./connection');
 const { ObjectID } = require('mongodb');
+const passport = require('passport');
 
 const app = express();
 let pug = require('pug');
-const passport = require('passport');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -27,33 +27,52 @@ app.use(
 passport.initialize();
 passport.session();
 
-// Save user id to a cookie
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-// retrieve user details from cookie
-passport.deserializeUser((id, done) => {
-  myDB.findOne(
-    {
-      _id: new ObjectID(id),
-    },
-    (err, doc) => {
-      done(null, null);
-    }
-  );
-});
-
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
-app.route('/').get((req, res) => {
-  req.session.count++;
-  console.log(req.session);
-  res.render('index', { title: 'Hello', message: 'Please log in' });
-});
+// use myDB to connect to the database and start server
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
-});
+myDB(async (client) => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Save user id to a cookie
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  // retrieve user details from cookie
+  passport.deserializeUser((id, done) => {
+    myDB.findOne(
+      {
+        _id: new ObjectID(id),
+      },
+      (err, doc) => {
+        done(null, doc);
+      }
+    );
+  });
+
+  app.route('/').get((req, res) => {
+    req.session.count++;
+    console.log(req.session);
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login',
+    });
+  });
+})
+  .catch((e) => {
+    console.error(e);
+    app.route('/').get((req, res) => {
+      res.render('index', {
+        title: e,
+        message: 'Unable to connect to database',
+      });
+    });
+  })
+  .finally(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log('Listening on port ' + PORT);
+    });
+  });
