c8c19c1d571a39491e4d39dd16c3c3889cd56968
E-K8 <ekatplat@gmail.com>
2024-05-02 20:50:14 +0100

    add authentication strategy

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const myDB = require('./connection');
 const { ObjectID } = require('mongodb');
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 const app = express();
 let pug = require('pug');
@@ -60,6 +61,22 @@ myDB(async (client) => {
       }
     );
   });
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne(
+        {
+          username: username,
+        },
+        (err, user) => {
+          console.log(`User ${username} attempted to log in.`);
+          if (err) return done(err);
+          if (!user) return done(null, false);
+          if (password !== user.password) return done(null, false);
+          return done(null, user);
+        }
+      );
+    })
+  );
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
