e9edb0869ae080db5ce3d039154746d0e965246f
E-K8 <ekatplat@gmail.com>
2024-05-08 21:36:49 +0100

    hash passwords with bcrypt

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const myDB = require('./connection');
 const { ObjectID } = require('mongodb');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -68,10 +69,11 @@ myDB(async (client) => {
         } else if (user) {
           res.redirect('/');
         } else {
+          const hash = bcrypt.hashSync(req.body.password, 12);
           myDataBase.insertOne(
             {
               username: req.body.username,
-              password: req.body.password,
+              password: hash,
             },
             (err, doc) => {
               if (err) {
@@ -121,7 +123,8 @@ myDB(async (client) => {
           console.log(`User ${username} attempted to log in.`);
           if (err) return done(err);
           if (!user) return done(null, false);
-          if (password !== user.password) return done(null, false);
+          if (!bcrypt.compareSync(password, user.password))
+            return done(null, false);
           return done(null, user);
         }
       );
