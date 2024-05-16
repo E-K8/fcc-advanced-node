410b59575d70aa62b0286815ac49c38bb4a2170c
E-K8 <ekatplat@gmail.com>
2024-05-16 22:30:40 +0100

    authenticate with Socket.IO

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ const app = express();
 // Create the HTTP server using the Express app
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
+const MongoStore = require('connect-mongo')(session);
+const cookieParser = require('cookie-parser');
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -29,6 +34,19 @@ app.use(
     resave: true,
     saveUninitialized: true,
     cookie: { secure: false },
+    key: 'express.sid',
+    store: store,
+  })
+);
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail,
   })
 );
 
@@ -47,7 +65,8 @@ myDB(async (client) => {
   io.on('connection', (socket) => {
     ++currentUsers;
     io.emit('user count', currentUsers);
-    console.log('A user has connected');
+    // console.log('A user has connected');
+    console.log('user ' + socket.request.user.username + ' connected');
 
     socket.on('disconnect', () => {
       console.log('A user has disconnected');
@@ -61,6 +80,17 @@ myDB(async (client) => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io', message);
+  accept(null, false);
+}
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
