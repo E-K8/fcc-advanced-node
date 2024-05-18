95211a545a3e71d1d0048f5b878bb12d7d3e341a
E-K8 <ekatplat@gmail.com>
2024-05-18 20:38:16 +0100

    add user connect/disconnect announcements and update user count

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const io = require('socket.io')(http);
 const passportSocketIo = require('passport.socketio');
 const MongoStore = require('connect-mongo')(session);
 const cookieParser = require('cookie-parser');
+const { connect } = require('http2');
 const URI = process.env.MONGO_URI;
 const store = new MongoStore({ url: URI });
 
@@ -64,14 +65,21 @@ myDB(async (client) => {
   let currentUsers = 0;
   io.on('connection', (socket) => {
     ++currentUsers;
-    io.emit('user count', currentUsers);
-    // console.log('A user has connected');
-    console.log('user ' + socket.request.user.username + ' connected');
+    io.emit('user', {
+      username: socket.request.user.username,
+      currentUsers,
+      connected: true,
+    });
+    console.log('A user has connected');
 
     socket.on('disconnect', () => {
       console.log('A user has disconnected');
       --currentUsers;
-      io.emit('user count', currentUsers);
+      io.emit('user', {
+        username: socket.request.user.username,
+        currentUsers,
+        connected: false,
+      });
     });
   });
 }).catch((e) => {
